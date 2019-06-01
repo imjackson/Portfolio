e1005a2c8e2fa16db2827dddf5a113b963945354
nbm-jackson <jacksongpack@gmail.com>
2019-06-01 15:14:06 -0400

    social media links

--- src/components/footer.js
+++ src/components/footer.js
@@ -21,9 +21,9 @@ const Footer = (props) => (
         </div>
 
         <div className={ s.FooterRight }>
-            <img alt="Instagram." className={ s.SocialIcon } src={insta} />
-            <img alt="Linkedin." className={ s.SocialIcon } src={linkedin} />
-            <img alt="Github." className={ s.SocialIcon } src={github} />
+            <a className={ s.FooterLink } href="https://www.instagram.com/jackson.pack/" target="_blank" ><img alt="Instagram." className={ s.SocialIcon } src={insta} /></a>
+            <a className={ s.FooterLink } href="https://www.linkedin.com/in/jackson-pack-08614b188/" target="_blank" ><img alt="Linkedin." className={ s.SocialIcon } src={linkedin} /></a>
+            <a className={ s.FooterLink } href="https://github.com/nbm-jackson" target="_blank" ><img alt="Github." className={ s.SocialIcon } src={github} /></a>
         </div>
     </footer>
 )
