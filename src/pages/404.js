aeb9bfecd88c905aeda500e60912b51d2beef3f4
nbm-jackson <jacksongpack@gmail.com>
2019-09-12 00:12:19 -0400

    moved 404 styling to its own css module file / updated 404 return focus behavior

--- src/pages/404.js
+++ src/pages/404.js
@@ -15,20 +15,21 @@ import Footer from '../components/footer'
 
 // Icons 
 import logo from '../images/logos/logo.svg'
+
 // Styles
-import s from '../modules/layout.module.css'
+import s from '../modules/notfound.module.css'
 
 const NotFoundPage = () => (
-  <div className={ s.NotFound }>
+  <div className={ s.PageContainer }>
     <SEO title="404: Not Found | Front-End Developer" description="I'm a front-end developer with a dedication to writing fast, simple, accessible websites." />
-    <header className={ s.NotFoundHeader }>
+    <header className={ s.Header }>
       <Link to="/"><img src={logo} alt="Logo." className={ s.Logo } /></Link>
     </header>
     <div className={ s.NotFoundContent }>
       <h2 className={ s.NotFoundTitle }>Uh oh,</h2>
       <h1 className={ s.NotFoundNumber }> <span className={ s.NotFoundNumberSmall}>a</span> 404 <span className={ s.NotFoundNumberSmall}>error.</span></h1>
       <p className={ s.NotFoundText }>It seems you've been led to a route that doesn't exist on my site. That's especially weird considering my site is only one page. No worries, just click the link below to view my site.</p>
-      <div className={ s.ButtonAdj }><Link to="/" className={ s.NotFoundReturn }>imjackson.dev &#8594;</Link></div>
+      <div className={ s.ButtonContainer }><Link to="/" className={ s.Button }>imjackson.dev &#8594;</Link></div>
     </div>
     
     <Footer />
