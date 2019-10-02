9d6b2998423620b92ceb2ace3382c7a2b313536b
imjackson <jacksongpack@gmail.com>
2019-10-02 13:50:27 -0400

    implemented formatOnSave

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,29 +7,44 @@
 
 // Dependencies
 import React from "react"
-import { Link } from 'gatsby'
+import { Link } from "gatsby"
 
 // Components
 import SEO from "../components/seo"
-import Footer from '../components/Footer/'
-import InternalHeader from '../components/InternalHeader/'
+import Footer from "../components/Footer/"
+import InternalHeader from "../components/InternalHeader/"
 
 // Styles
-import s from '../modules/notfound.module.css'
+import s from "../modules/notfound.module.css"
 
 const NotFoundPage = () => (
-  <div className={ s.PageContainer }>
-    <SEO title="404: Not Found | Front-End Developer" description="I'm a front-end developer with a dedication to writing fast, simple, accessible websites." />
-    <InternalHeader />
-    <div className={ s.NotFoundContent }>
-      <h2 className={ s.NotFoundTitle }>Uh oh,</h2>
-      <h1 className={ s.NotFoundNumber }> <span className={ s.NotFoundNumberSmall}>a</span> 404 <span className={ s.NotFoundNumberSmall}>error.</span></h1>
-      <p className={ s.NotFoundText }>It seems you've been led to a route that doesn't exist on my site. That's especially weird considering my site is only one page. No worries, just click the link below to view my site.</p>
-      <div className={ s.ButtonContainer }><Link to="/" className={ s.Button }>imjackson.dev &#8594;</Link></div>
+    <div className={s.PageContainer}>
+        <SEO
+            title="404: Not Found | Front-End Developer"
+            description="I'm a front-end developer with a dedication to writing fast, simple, accessible websites."
+        />
+        <InternalHeader />
+        <div className={s.NotFoundContent}>
+            <h2 className={s.NotFoundTitle}>Uh oh,</h2>
+            <h1 className={s.NotFoundNumber}>
+                {" "}
+                <span className={s.NotFoundNumberSmall}>a</span> 404{" "}
+                <span className={s.NotFoundNumberSmall}>error.</span>
+            </h1>
+            <p className={s.NotFoundText}>
+                It seems you've been led to a route that doesn't exist on my
+                site. That's especially weird considering my site is only one
+                page. No worries, just click the link below to view my site.
+            </p>
+            <div className={s.ButtonContainer}>
+                <Link to="/" className={s.Button}>
+                    imjackson.dev &#8594;
+                </Link>
+            </div>
+        </div>
+
+        <Footer />
     </div>
-    
-    <Footer />
-  </div>
 )
 
 export default NotFoundPage
