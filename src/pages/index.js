fe9a9944cfd0225211f8ce8434f4bff8a699a45f
imjackson <jacksongpack@gmail.com>
2020-02-27 18:16:02 -0500

    refactored index.js page to use fuctional component / hooks

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@
 // ============================================================
 
 // dependencies
-import React from "react"
+import React, { useState, useEffect, useRef } from "react"
 import { graphql } from "gatsby"
 
 // Components
@@ -19,52 +19,37 @@ import Work from "../components/Work/"
 import Contact from "../components/Contact/"
 
 // Styles
-import s from "../modules/index.module.css"
+import styles from "../modules/index.module.css"
 
-// Other Icons
-import down from "../images/down-arrow.svg"
+const Index = props => {
+    // refs
+    const IntroSection = useRef(null)
+    const AboutSetion = useRef(null)
+    const WorkSection = useRef(null)
+    const ContactSection = useRef(null)
 
-export default class Index extends React.Component {
-    constructor(props) {
-        super(props)
-        // refs
-        this.Splash = React.createRef()
-        this.About = React.createRef()
-        this.Work = React.createRef()
-        this.Contact = React.createRef()
+    const [headerIsActive, setHeaderIsActive] = useState(false)
+    const [currentSection, setCurrentSection] = useState("intro")
 
-        // initial state
-        this.state = {
-            activeHeader: false,
-            sec: "splash",
-            formName: "",
-            formEmail: "",
-            formMessage: "",
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll)
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
         }
-    }
-
-    // add scroll listener to window
-    componentDidMount() {
-        window.addEventListener("scroll", this.handleScroll)
-    }
-
-    // remove scroll listener from window
-    componentWillUnmount() {
-        window.removeEventListener("scroll", this.handleScroll)
-    }
+    }, [])
 
     // scroll listener - determines which section user is currently viewing, allowing for dyanmic styling
     // of header link items
-    handleScroll = () => {
+    const handleScroll = () => {
         let y = window.pageYOffset,
             a,
             s
-        let splash = this.Splash.current.offsetTop,
-            about = this.About.current.offsetTop,
-            work = this.Work.current.offsetTop,
-            contact = this.Contact.current.offsetTop
+        let intro = IntroSection.current.offsetTop,
+            about = AboutSetion.current.offsetTop,
+            work = WorkSection.current.offsetTop,
+            contact = ContactSection.current.offsetTop
         if (
-            y >= about - this.Splash.current.offsetHeight / 4 &&
+            y >= about - IntroSection.current.offsetHeight / 4 &&
             y < work - about / 4
         ) {
             s = "about"
@@ -73,24 +58,22 @@ export default class Index extends React.Component {
         } else if (y >= contact - work / 4) {
             s = "contact"
         } else {
-            s = "splash"
+            s = "intro"
         }
         y > 40 ? (a = true) : (a = false) // Header styling logic
-        this.setState({
-            activeHeader: a,
-            sec: s,
-        })
+        setHeaderIsActive(a)
+        setCurrentSection(s)
     }
 
     // scrolls to specific section of page based of prop 't'
-    scroller = t => {
-        let splash = this.Splash.current.offsetTop,
-            about = this.About.current.offsetTop,
-            work = this.Work.current.offsetTop,
-            contact = this.Contact.current.offsetTop
+    const scroller = t => {
+        let intro = IntroSection.current.offsetTop,
+            about = AboutSetion.current.offsetTop,
+            work = WorkSection.current.offsetTop,
+            contact = ContactSection.current.offsetTop
         let to
         t === "splash"
-            ? (to = splash)
+            ? (to = intro)
             : t === "about"
             ? (to = about)
             : t === "work"
@@ -98,55 +81,51 @@ export default class Index extends React.Component {
             : t === "contact"
             ? (to = contact)
             : (to = "error")
-        window.scrollTo({ top: to - this.getRem(6), behavior: "smooth" })
+        window.scrollTo({ top: to - getRem(6), behavior: "smooth" })
         setTimeout(() => {
-            this.setState({ sec: t })
+            setCurrentSection(t)
         }, 1000)
     }
 
     // scroll to top of window
-    toTop = () => {
+    const toTop = () => {
         window.scrollTo({ top: 0, behavior: "smooth" })
     }
 
     // convert from rem value to pixel value based on document fontSize
-    getRem = rem => {
+    const getRem = rem => {
         return (
             rem *
             parseFloat(getComputedStyle(document.documentElement).fontSize)
         )
     }
 
-    render() {
-        return (
-            <Layout
-                top={this.toTop}
-                scroller={this.scroller}
-                activeSection={this.state.sec}
-                activeHeader={this.state.activeHeader}
-            >
-                <SEO
-                    title="Front-End Developer"
-                    description="I'm a front-end developer with a dedication to writing fast, simple, accessible websites."
-                />
+    return (
+        <Layout
+            top={toTop}
+            scroller={scroller}
+            activeSection={currentSection}
+            activeHeader={headerIsActive}
+        >
+            <SEO
+                title="Front-End Developer"
+                description="I'm a front-end developer with a dedication to writing fast, simple, accessible websites."
+            />
 
-                <div className={s.Main}>
-                    <Intro ref={this.Splash} scroller={this.scroller} />
-                    <About ref={this.About} />
-                    <Work
-                        finalsClubImage={
-                            this.props.data.finalsclub.childImageSharp.fluid
-                        }
-                        rocklandImage={
-                            this.props.data.rockland.childImageSharp.fluid
-                        }
-                        ref={this.Work}
-                    />
-                    <Contact ref={this.Contact} />
-                </div>
-            </Layout>
-        )
-    }
+            <div className={styles.Main}>
+                <Intro ref={IntroSection} scroller={scroller} />
+                <About ref={AboutSetion} />
+                <Work
+                    finalsClubImage={
+                        props.data.finalsclub.childImageSharp.fluid
+                    }
+                    rocklandImage={props.data.rockland.childImageSharp.fluid}
+                    ref={WorkSection}
+                />
+                <Contact ref={ContactSection} />
+            </div>
+        </Layout>
+    )
 }
 
 // graphql query fragment for getting fluid images
@@ -171,3 +150,4 @@ export const pageQuery = graphql`
         }
     }
 `
+export default Index
