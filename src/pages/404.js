8881e4781f7f7e3a810d7e0cb5f0ecb383dae9ea
imjackson <jacksongpack@gmail.com>
2020-03-04 13:58:33 -0500

    cleaned up naming in 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -15,29 +15,29 @@ import Footer from "../components/Footer/"
 import InternalHeader from "../components/InternalHeader/"
 
 // Styles
-import s from "../modules/notfound.module.css"
+import styles from "../modules/notfound.module.css"
 
 const NotFoundPage = () => (
-    <div className={s.PageContainer}>
+    <div className={styles.PageContainer}>
         <SEO
             title="404: Not Found | Front-End Developer"
             description="I'm a front-end developer with a dedication to writing fast, simple, accessible websites."
         />
         <InternalHeader />
-        <div className={s.NotFoundContent}>
-            <h2 className={s.NotFoundTitle}>Uh oh,</h2>
-            <h1 className={s.NotFoundNumber}>
+        <div className={styles.NotFoundContent}>
+            <h2 className={styles.NotFoundTitle}>Uh oh,</h2>
+            <h1 className={styles.NotFoundNumber}>
                 {" "}
-                <span className={s.NotFoundNumberSmall}>a</span> 404{" "}
-                <span className={s.NotFoundNumberSmall}>error.</span>
+                <span className={styles.NotFoundNumberSmall}>a</span> 404{" "}
+                <span className={styles.NotFoundNumberSmall}>error.</span>
             </h1>
-            <p className={s.NotFoundText}>
+            <p className={styles.NotFoundText}>
                 It seems you've been led to a route that doesn't exist on my
                 site. That's especially weird considering my site is only one
                 page. No worries, just click the link below to view my site.
             </p>
-            <div className={s.ButtonContainer}>
-                <Link to="/" className={s.Button}>
+            <div className={styles.ButtonContainer}>
+                <Link to="/" className={styles.Button}>
                     imjackson.dev &#8594;
                 </Link>
             </div>
