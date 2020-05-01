0636497a1be050aac7c69498a9bca84a7761e2d0
imjackson <jacksongpack@gmail.com>
2020-05-01 17:51:20 -0400

    [package.json] switch to preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     plugins: [
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-sitemap`,
+        `gatsby-plugin-preact`,
         {
             resolve: `gatsby-source-filesystem`,
             options: {
